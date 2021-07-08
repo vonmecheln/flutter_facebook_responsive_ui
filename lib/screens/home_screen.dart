6bbd9c7d37d59b8652a0fec9321ba12404e1d573
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-07-07 21:44:05 -0300

    Rooms

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,6 +43,12 @@ class HomeScreen extends StatelessWidget {
             child: CreatePostContainer(
               currentUser: currentUser,
             ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: Rooms(onlineUsers: onlineUsers),
+            ),
           )
         ],
       ),
